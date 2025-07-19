c949d3f4d536b7b17c6fe5799bb7e5edb4ddad72
YanNeumann <koya12355@gmail.com>
2025-07-19 19:03:51 +0300

    start

---
+++ src/main/java/com/boardclub/eventcalendar/EventCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.boardclub.eventcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boardclub/eventcalendar/EventCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardclub.eventcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
